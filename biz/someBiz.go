ba2d2cc2128f9db1984e9076a1d568d024e6029b
qizhenyan <qizhenyan@bytedance.com>
2020-03-31 10:41:44 +0800

    对法国红酒

---
+++ biz/someBiz.go
@@ -0,0 +1,11 @@
+package biz
+
+func GetAns(a,b int) int{
+	return a*b
+}
+func GetAns2(a,b int) int{
+	return a*b
+}
+func GetAns3(a,b int) int{
+	return a*b
+}
---
+++ main/HelloWorld.go
@@ -0,0 +1,17 @@
+package main
+
+import _"awesomeProject/biz"
+//func main(){
+//	fmt.Println("Hello world!")
+//}
+func GetSum(a,b int)  int{
+	sum:=a+b
+	if a>10{
+		sum=a+b
+
+	}
+	//sum=biz.GetAns(a,b)
+	sum=a+b
+
+	return sum
+}
