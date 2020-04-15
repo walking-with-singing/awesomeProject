b8579dc4eec00169b54b4b566eba35ffb955ccb7
qizhenyan <qizhenyan@bytedance.com>
2020-04-15 21:09:08 +0800

    4月15日-master

--- main/HelloWorld.go
+++ main/HelloWorld.go
@@ -11,9 +11,9 @@ func main() {
 	for i:=0;i<10000000000;i++{
 		count+=i
 	}
+	fmt.Println("master")
 	t2:=time.Now()
 	tsub:=now.Sub(t2)
 	fmt.Println(tsub)
 	fmt.Println(int32(tsub.Seconds()))
-	fmt.Println("master")
 }
